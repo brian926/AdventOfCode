85d8148e518609b7ac7d423926a186e2fcce6bdf
Brian Antunes <brian.antunes@yahoo.com>
2022-12-04 10:19:33 -0500

    Finished Day 4 part 2

--- Day 4/main.go
+++ Day 4/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	readFile, err := os.ReadFile("assign2.txt")
+	readFile, err := os.ReadFile("assign.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -18,31 +18,38 @@ func main() {
 
 	contains := 0
 	dups := 0
+	var tests [][]string
 
 	for _, line := range temp {
-		test := strings.Split(line, ",")
+		tests = append(tests, strings.Split(line, ","))
+	}
+
+	for _, test := range tests {
 		firstH := strings.Split(test[0], "-")
 		secondH := strings.Split(test[1], "-")
 
 		min1, _ := strconv.Atoi(firstH[0])
 		max1, _ := strconv.Atoi(firstH[1])
 		min2, _ := strconv.Atoi(secondH[0])
-		max2, _ := strconv.Atoi(secondH[1])
+		max2, _ := strconv.Atoi(strings.TrimRight(secondH[1], "\r"))
 
+		fmt.Println(max2)
 		if (min1 >= min2 && max1 <= max2) || (min2 >= min1 && max2 <= max1) {
 			contains += 1
 
 		}
 		if overLap(min1, max1, min2, max2) {
-			fmt.Println(firstH, secondH)
 			dups += 1
 		}
 	}
+
 	fmt.Println("Fully contains:", contains)
 	fmt.Println("Dup count:", dups)
 }
 
 func overLap(min1, max1, min2, max2 int) bool {
+
+	//fmt.Println(min1, min2, (min1 >= min2), min1, max2, (min1 <= max2))
 	if min1 >= min2 && min1 <= max2 {
 		return true
 	}
